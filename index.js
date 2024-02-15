b09290922ff76da3d63d43b4c4ce0341b1eed2a7
TimotheSymbolIt <timothe.gauquelin@symbol-it.fr>
2024-02-15 15:57:51 +0100

    Ajout des fonctions javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+function removeAd() {
+	document.getElementById("ad").style.display = "none";
+}
+
+function toggleNavbar() {
+	document.getElementById("navbar").classList.toggle("displayNavbar");
+}
